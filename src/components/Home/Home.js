9197d5a62bfb5706072432075da6f40106711e9b
Rasheduzzaman Sifat <me.sifat@live.com>
2025-05-24 06:04:23 +0800

    font change

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -22,7 +22,7 @@ function Home() {
 
               <h1 className="heading-name">
                 We're
-                <strong className="main-name"> Eon Tech Solutions</strong>
+                <strong className="main-name"> EonTechs</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
