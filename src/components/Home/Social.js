1120db5e5be3a96a24b719ffc8626e18b6c5f38b
Rasheduzzaman Sifat <me.sifat@live.com>
2025-09-04 15:18:39 +0800

    update again

--- src/components/Home/Social.js
+++ src/components/Home/Social.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { AiFillGithub } from "react-icons/ai";
-import { FaLinkedinIn, FaFacebookF } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa";
 
 function SocialContact() {
   return (
